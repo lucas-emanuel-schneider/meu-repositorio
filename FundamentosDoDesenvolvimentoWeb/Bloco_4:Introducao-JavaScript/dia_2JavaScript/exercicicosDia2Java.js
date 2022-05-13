eabd67920f22f7070adb3c58b357f73f78b46013
Lucas <lucas_emanuel_sc@hotmail.com>
2022-05-13 19:02:12 -0300

    Alguns dos exercicicos

---
+++ FundamentosDoDesenvolvimentoWeb/Bloco_4:Introducao-JavaScript/dia_2JavaScript/exercicicosDia2Java.js
@@ -0,0 +1,27 @@
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   console.log(numbers[index])
+// }
+// NR 2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0
+// for (let index = 0; index < numbers.length; index += 1) {
+  
+//   soma += numbers[index];
+// } console.log(soma);
+// // NR 3
+// mediaAritmetica = soma / numbers.length
+// console.log(mediaAritmetica)
+// if (mediaAritmetica > 20) {
+//   console.log("Valor Maior Que 20!")
+// } else if (mediaAritmetica <= 20) {
+//   console.log('Valor Menor ou Igual a 20!');
+// // } NR 4
+// let maior = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//    if (numbers[i] > maior) {
+//       maior = numbers[i];
+//    }
+// } console.log(maior)
