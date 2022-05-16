2d15aa1f72f9076060cf2bfa4012c6d950136adc
Lucas <lucas_emanuel_sc@hotmail.com>
2022-05-16 19:46:58 -0300

    Exercicios do 4.3

--- FundamentosDoDesenvolvimentoWeb/Bloco_4:Introducao-JavaScript/dia_3JavaScript_logicaDeProgramacao/dia_3JavaScript_logicaDeProgramacao.js
+++ FundamentosDoDesenvolvimentoWeb/Bloco_4:Introducao-JavaScript/dia_3JavaScript_logicaDeProgramacao/dia_3JavaScript_logicaDeProgramacao.js
@@ -8,3 +8,39 @@
 //   resultado *=  i;
 // }
 // console.log(resultado);
+// 2 =>
+// let word = 'pimenta';
+// let palavraContraria = '';
+// for (let i = word.length - 1; i >= 0; i -= 1 ) {
+//   palavraContraria += word[i] ;
+// }
+// console.log(palavraContraria);
+// 3 =>
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorValor = array[0];
+// for (let i = 0; i <= array.length - 1; i += 1) {
+//   if (array[i].length > maiorValor.length) {
+//     maiorValor = array[i]
+//   }
+// }
+// console.log(maiorValor)
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let menorValor = array[0];
+// for (let i = 0; i <= array.length - 1; i += 1) {
+//   if (array[i].length < menorValor.length) {
+//     menorValor = array[i]
+//   }
+// }
+// console.log(menorValor)
+// 4 =>
+let numeros = 50;
+let numerosPrimos = 0;
+for (let i = 2; i <= numeros; i += 1) {
+  for (let i2 = 2; i2 < i; i2 += 1) {
+    if (numeros[i] % numeros[i2] != 0) {
+      let numerosPrimos = numeros[i];
+      } 
+    }
+    console.log(numerosPrimos)
+  }
+
