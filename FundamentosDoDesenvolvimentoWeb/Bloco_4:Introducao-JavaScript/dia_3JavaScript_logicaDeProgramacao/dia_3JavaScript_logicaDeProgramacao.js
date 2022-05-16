f4d4c2dae7a972e9893ad66d2653b285d01054b1
Lucas <lucas_emanuel_sc@hotmail.com>
2022-05-16 10:53:25 -0300

    exercicios 4.3 o de vetorial

---
+++ FundamentosDoDesenvolvimentoWeb/Bloco_4:Introducao-JavaScript/dia_3JavaScript_logicaDeProgramacao/dia_3JavaScript_logicaDeProgramacao.js
@@ -0,0 +1,10 @@
+// 4! = 4 x 3 x 2 x 1 = 24
+// VETORIAL SUPER FACIL NEM DEMOREI QUASE, só 1 hr...
+// let vetorial = 10;
+// let resultado = vetorial;
+// primeiroMultiplicador = vetorial - 1;
+
+// for (let i = primeiroMultiplicador; i > 1; i -- ) {
+//   resultado *=  i;
+// }
+// console.log(resultado);
