cfdf94862de553adb3ede63cdd1a8d137a80bc5a
Lucas <lucas_emanuel_sc@hotmail.com>
2022-05-17 15:12:01 -0300

    exemplos de hoje

---
+++ FundamentosDoDesenvolvimentoWeb/Bloco_4:Introducao-JavaScript/exercicios_dia4.4Js.js
@@ -0,0 +1,10 @@
+// let player = {
+// name: 'Marta',
+// lastName: 'Silva',
+// age: 34,
+// bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+// medals: { golden: 2, silver: 3, },
+// }
+// console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' ' + 'tem ' + player['age'] + ' ' + 'anos de idade.');
+// console.log( 'A jogadora Marta foi eleita a melhor do mundo ' + player.bestInTheWorld.length + ' vezes.');
+// console.log( 'A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' de prata.');
