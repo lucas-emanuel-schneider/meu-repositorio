c3b39e6b552e336957703d7dc7d945dda9b337b4
Lucas <lucas_emanuel_sc@hotmail.com>
2022-05-19 14:47:31 -0300

    alguma coisa que eu n tinha comitado no dia anterior... hahaha

--- FundamentosDoDesenvolvimentoWeb/Bloco_4:Introducao-JavaScript/exercicios_dia4.4Js.js
+++ FundamentosDoDesenvolvimentoWeb/Bloco_4:Introducao-JavaScript/exercicios_dia4.4Js.js
@@ -27,14 +27,14 @@
 //   console.log(key, car[key]);
 // }
 // functions !
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-};
-// console.log("Bem vinda, " + info.personagem + "!");
-// nr 2
-info.recorrente = 'Sim';
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+// // console.log("Bem vinda, " + info.personagem + "!");
+// // nr 2
+// info.recorrente = 'Sim';
 // for (let key in info) {
 //   console.log(key);
 // }
@@ -55,30 +55,62 @@ info.recorrente = 'Sim';
 //   } else {
 //   console.log(info[key] + ' e ' + info2[key])
 //   }
-// }
-// 6
-let leitor = {
-  nome: 'Julia',
-  sobrenome: 'Pessoa',
-  idade: 21,
-  livrosFavoritos: [
-    {
-      titulo: 'O Pior Dia de Todos',
-      autor: 'Daniela Kopsch',
-      editora: 'Tordesilhas',
-    },
-  ],
-};
+// // }
+// // 6
+// let leitor = {
+//   nome: 'Julia',
+//   sobrenome: 'Pessoa',
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: 'O Pior Dia de Todos',
+//       autor: 'Daniela Kopsch',
+//       editora: 'Tordesilhas',
+//     },
+//   ],
+// };
 // console.log('"O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo + '"')
-// 7
-leitor.livrosFavoritos.push({
-  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-  autor: 'JK Rowling',
-  editora: 'Rocco',
-})
-console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');
+// // 7
+// leitor.livrosFavoritos.push({
+//   titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//   autor: 'JK Rowling',
+//   editora: 'Rocco',
+// })
+// console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');
+// 1 =>
+// function verifyPalindromo(palavra) {
+//  let palindromo = '';
+// for (i = palavra.length - 1; i >= 0; i -= 1) {
+//   palindromo = palindromo + palavra[i];
+// }
+// if (palavra == palindromo) {
+//   return true;
+// } else { return  false;}
 
-
+// }
+// let exemplo = 'competicao';
+// console.log(verifyPalindromo(exemplo));
+// 5 >
+function maisRepetido(numeros) {
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+  for (let index in numeros) {
+    for (let index2 in numeros) {
+      if (numeros[index] === numeros[index2]) {
+        contNumero += 1;
+      }
+    }
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
+    }
+    contNumero = 0;
+  }
+  return numeros[indexNumeroRepetido];
+}
+let test = [2, 3, 2, 5, 8, 2, 3];
+console.log(maisRepetido(test));
 
 
 
