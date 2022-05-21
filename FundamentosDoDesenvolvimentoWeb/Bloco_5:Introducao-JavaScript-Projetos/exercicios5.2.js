f46dfa7bc8945d7878418209d20fe651df54b528
Lucas <lucas_emanuel_sc@hotmail.com>
2022-05-20 21:07:47 -0300

    exercicios de hoje

--- FundamentosDoDesenvolvimentoWeb/Bloco_5:Introducao-JavaScript-Projetos/exercicios5.2.js
+++ FundamentosDoDesenvolvimentoWeb/Bloco_5:Introducao-JavaScript-Projetos/exercicios5.2.js
@@ -14,5 +14,27 @@ document.querySelector('main').appendChild(sectionCenter);
 let paragraph = document.createElement('p');
 paragraph.innerText = 'Texto Dentro Do Paragráfo!'
 document.querySelector('.center-content').appendChild(paragraph);
+// 5
+let section2 = document.createElement('section');
+section2.className = 'left-content';
+document.querySelector('main').appendChild(section2);
+// 6
+let section3 = document.createElement('section');
+section3.className = 'right-content';
+document.querySelector('main').appendChild(section3);
+// 7
+let imagem = document.createElement('img');
+imagem.className = 'small-image';
+imagem.src = 'https://picsum.photos/200';
+document.querySelector('.left-content').appendChild(imagem);
+// 8
+let listaUl = document.createElement('ul');
+let listaDeLis = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez']
+document.querySelector('section3').appendChild('listaUl');
+for (let i = 0; i < listaUl.length; i += 1) {
+  let linha = document.createElement('li');
+  linha.innerText = listaDeLis[i];
+  document.querySelector('listaUl').appendChild('linha');
+}
 
 
