6f86c751b98b5ff4ad6bf030b527265ecce374a2
Lucas <lucas_emanuel_sc@hotmail.com>
2022-06-09 10:18:21 -0300

    tentei ate parar de tentar

--- FundamentosDoDesenvolvimentoWeb/Bloco_7:JavaScript_ES6-TestesUnitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script7/1.js
+++ FundamentosDoDesenvolvimentoWeb/Bloco_7:JavaScript_ES6-TestesUnitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script7/1.js
@@ -18,3 +18,35 @@
 // const returnArray = (oddsAndEvens) => oddsAndEvens.sort((a, b) => a - b);
 
 // console.log(`Os números ${returnArray(oddsAndEvens)} se encontram ordenados de forma crescente!`);
+// EX 3
+// const factorialFunc = n => (n > 1) ? n * factorialFunc(n - 1) : 1;
+// console.log(factorialFunc(4));
+// EX 4
+// const array = ["surtar", "codar", "perseverar", "conseguir", "novamente"];
+
+// function buildSkillsPhrase (paramOne) {
+//     const fun1 = paramInner => (
+//       `Tryber ${paramInner} aqui!
+
+//       Tudo bem?`
+//     );
+
+//     let result = `${fun1(paramOne)}
+
+//     Minhas cinco principais habilidades são:`;
+
+//     array.forEach((skill, index) =>
+//     result = `${result}
+
+//     - ${skill}`);
+
+//     result = `
+// ${result}
+
+//     #goTrybe
+//     `;
+
+//     return result;
+// }
+
+// console.log(buildSkillsPhrase("Lucas"));
