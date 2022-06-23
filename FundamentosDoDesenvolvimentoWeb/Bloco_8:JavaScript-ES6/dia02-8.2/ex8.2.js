112336b1ff80070ddbd2a0bc1fa0fdfb03bc1240
Lucas <lucas_emanuel_sc@hotmail.com>
2022-06-23 15:38:04 -0300

    exercicios de fixar

---
+++ FundamentosDoDesenvolvimentoWeb/Bloco_8:JavaScript-ES6/dia02-8.2/ex8.2.js
@@ -0,0 +1,24 @@
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// const findDivisibleBy3And5 = (number) => {
+//   if (number % 3 === 0 && number % 5 === 0) return number;
+// }
+
+// console.log(numbers.find(findDivisibleBy3And5));
+// 2 fixar
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// const findNameWithFiveLetters = (name) => name.length === 5;
+
+// console.log(names.find(findNameWithFiveLetters));
+// 3
+// const musicas = [
+//   { id: '31031685', title: 'Partita in C moll BWV 997' },
+//   { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+//   { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+// ]
+
+// const findMusic = (id) => musicas.find((musica) => musica.id === id); 
+
+
+// console.log(findMusic('31031685'))
