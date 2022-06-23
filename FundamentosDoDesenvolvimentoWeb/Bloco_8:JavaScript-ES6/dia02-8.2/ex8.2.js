0922057363c3e59c9b4056ed174e862f9a41b807
Lucas <lucas_emanuel_sc@hotmail.com>
2022-06-23 19:36:07 -0300

    exercicios 3

--- FundamentosDoDesenvolvimentoWeb/Bloco_8:JavaScript-ES6/dia02-8.2/ex8.2.js
+++ FundamentosDoDesenvolvimentoWeb/Bloco_8:JavaScript-ES6/dia02-8.2/ex8.2.js
@@ -1,8 +1,6 @@
 // const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-// const findDivisibleBy3And5 = (number) => {
-//   if (number % 3 === 0 && number % 5 === 0) return number;
-// }
+// const findDivisibleBy3And5 = (number) => (number % 3 === 0 && number % 5 === 0);
 
 // console.log(numbers.find(findDivisibleBy3And5));
 // 2 fixar
@@ -40,3 +38,83 @@
 // const verifyAges = (arr, minimumAge) => arr.every((person) => person.age >= minimumAge);
 
 // console.log(verifyAges(people, 18));
+// exercicios do dia
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// // Adicione o código do exercício aqui:
+// const authorBornIn1947 = books.find((book) => book.author.birthYear === 1947).author.name;
+// console.log(authorBornIn1947);
+// 2
+// function smallerName(books) {
+//   let smallName = books[0].name;
+//   books.forEach(book => {
+//     if (book.name.length < smallName.length) {
+//       smallName = book.name;
+//     }
+//   });
+//   return smallName;
+// }
+// console.log(smallerName(books));
+// 3
+
