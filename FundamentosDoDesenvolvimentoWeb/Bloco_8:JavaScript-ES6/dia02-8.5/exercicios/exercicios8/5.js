6ed0c7fb083625aaed5245340cff20606e800b3a
Lucas <lucas_emanuel_sc@hotmail.com>
2022-06-28 16:13:37 -0300

    terminei os para fixar

---
+++ FundamentosDoDesenvolvimentoWeb/Bloco_8:JavaScript-ES6/dia02-8.5/exercicios/exercicios8/5.js
@@ -0,0 +1,51 @@
+// // Faça uma lista com as suas frutas favoritas
+// const specialFruit = ['banana', 'uva', 'morango'];
+
+// // Faça uma lista de complementos que você gostaria de adicionar
+// const additionalItens = ['leite Ninho', 'leite condensado', 'granola'];
+
+// const fruitSalad = (fruit, additional) => [...fruit, ...additional];
+
+// console.log(fruitSalad(specialFruit, additionalItens));
+
+
+// const daysOfWeek = {
+//   workDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+//   weekend: ['Saturday', 'Sunday'],
+// };
+
+// const { workDays, weekend } = daysOfWeek;
+// console.log(workDays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+// console.log(weekend); // ['Saturday', 'Sunday']
+
+// const weekdays = [...workDays, ...weekend];
+// console.log(weekdays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+// const student = {
+//   a: 'Maria',
+//   b: 'Turma B',
+//   c: 'Matematica',
+// };
+
+// const { a: name, b: classAssigned, c: subject } = student;
+
+// console.log(name); // Maria
+// console.log(classAssigned); // Turma B
+// console.log(subject); // Matemática
+// para fixar
+
+// const user = {
+//   name: 'Maria',
+//   age: 21,
+//   nationality: 'Brazilian',
+// };
+
+// const jobInfos = {
+//   profession: 'Software engineer',
+//   squad: 'Rocket Landing Logic',
+//   squadInitials: 'RLL',
+// };
+// const person = {...user,
+// ...jobInfos,}
+
+// const {name: nome, age: idade, nationality: nacionalidade, profession: profissao, squad: squad} = person;
+// console.log(`Hi, my name is ${nome}, i'm ${idade} years old and i'm ${nacionalidade}. I work as a ${profissao} and my squad is ${squad}`);
