40c096ba4ecb229fa3b3b56e342a3c99d4bf9bed
Lucas <lucas_emanuel_sc@hotmail.com>
2023-01-12 10:48:07 -0300

    organizando

---
+++ DesenvolvimentoBack-end/Back1AlgumasPastasDeExercicios/typeScriptExpress/routes/books.routes.ts
@@ -0,0 +1,17 @@
+// ./routes/books.routes.ts
+
+import { Router } from 'express';
+import BooksController from '../controllers/books.controller';
+import validationBook from '../middlewares/books.middleware';
+
+const router = Router();
+
+const booksController = new BooksController();
+
+router.get('/books', booksController.getAll);
+
+router.get('/books/:id', booksController.getById);
+
+router.post('/books', booksController.create);
+
+export default router;
